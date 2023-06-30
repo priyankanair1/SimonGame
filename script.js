0a0b6f951c326f7f95a13d7db78a79a2fb68ae83
priyankanair1 <priyankanairammu90@gmail.com>
2023-06-29 18:04:28 -0700

    Add audio

--- script.js
+++ script.js
@@ -5,6 +5,21 @@ const Choices = {
   '2': "color_2",
   '3': "color_3"
 }
+
+const messages = {
+  "watch": "WATCH",
+  "play": "PLAY",
+  "start": "Click START to play",
+  "fail": "Failed! Click START to play again"
+}
+
+const audios = {
+  "0": "audio_0",
+  "1": "audio_1",
+  "2": "audio_2",
+  "3": "audio_3",
+  "fail": "audio_fail"
+}
 /*----- state variables -----*/
 let level;
 let started;
@@ -13,12 +28,18 @@ let clickCount;
 let blinkCount;
 let accurateClick;
 let intervalId;
+let playerTurn;
+let score;
+let highScore;
+let failed;
+
 /*----- cached elements  -----*/
 const startButton = document.getElementById('start')
 const redEl = document.getElementById('color_0');
 const blueEl = document.getElementById('color_1');
 const yellowEl = document.getElementById('color_2');
 const greenEl = document.getElementById('color_3');
+
 /*----- event listeners -----*/
 startButton.addEventListener('click', startGame);
 redEl.addEventListener('click', handleClick);
@@ -31,28 +52,54 @@ blueEl.addEventListener('click', handleClick);
 init();
 
 function init() {
-  level = 1;
+  level = 0;
   started = false;
   blinkedColors = [];
   clickCount = 0;
   accurateClick = false;
   blinkCount=0;
   intervalId = null;
+  playerTurn = false;
+  score = 0
+  highScore = 0
+  failed = false;
   render();
 }
 
-function render() {
-  document.getElementById("start").disabled = false;
-  setLevel(level);
+function reset() {
+  level = 0;
+  started = false;
+  blinkedColors = [];
+  clickCount = 0;
+  accurateClick = false;
+  blinkCount=0;
+  intervalId = null;
+  playerTurn = false;
+  score = 0
+  render();
+  document.getElementById("start").classList.remove("startDisable");  
 }
 
-function setLevel() {
-  document.getElementById("level").innerHTML = level;
+function render() {
+  clearInterval(intervalId);
+  document.getElementById("start").disabled = false;  
+  if(!failed) {
+    showMessage(messages["start"]);
+  }
+  failed = false;
+}
+
+function showMessage(message) {
+  document.getElementById("message").innerHTML = message;
+}
+
+function showScore() {
+  document.getElementById("score").innerHTML = "Score : "+score;
+  document.getElementById("highscore").innerHTML = "High Score : " + highScore;
 }
 
 function getNextColor() {
   let color = Math.floor(Math.random() * 4);
-  console.log("Color index : " + color);
   blinkedColors.push(color);
   blink(Choices[color]);
   return color;
@@ -62,50 +109,66 @@ function clearBlink(id) {
   document.getElementById(id).classList.remove("blink");
 }
 
+function playAudio(id) {
+  var audio = document.getElementById(audios[id]);
+  audio.play();
+}
+
 function blink(id) {
   document.getElementById(id).classList.add("blink");
+  playAudio(id.slice(6));
   blinkCount++;
   if(blinkCount == level && intervalId != null)  {
     clearInterval(intervalId);
+    showMessage(messages["play"]);
   }   
   setTimeout(clearBlink, 500, id);
 }
 
 function startGame() {
-  console.log("Game started !");
   started = true;
+  level = 1;
+  score = 0;
+  showMessage(messages["watch"]);
+  showScore();
   document.getElementById("start").disabled = true;
+  document.getElementById("start").classList.add("startDisable");
   getNextColor();
+  showMessage(messages["play"]);
+}
+
+function loseGame() {
+  failed = true;
+  started = false;
+  playAudio("fail");
+  showMessage(messages["fail"]);  
+  reset();
 }
 
 function handleClick(evt) {
   if(started) {
-    console.log("-------------------------------------");
     target = evt.target;
     let index = document.getElementById(evt.target.id).id.slice(6);
-    console.log("Clicked index : " + index);
-
+    playAudio(index);
     if(index == blinkedColors[clickCount]) {
       accurateClick = true;
     } else {
-      console.log("fail...");
-      init();
+      loseGame();
       return;
-      //http://codeskulptor-demos.commondatastorage.googleapis.com/descent/bomb.mp3
     }
-    console.log("accurateClick : " + accurateClick);
     clickCount++;
-    console.log("clickCount : " + clickCount);
-    console.log("level : " + level);
     if(clickCount == level) {
-      console.log("Finish level : " + level);
       blinkedColors = [];
       clickCount = 0;
-      blinkCount = 0;
+      blinkCount = 0;      
       intervalId = setInterval(getNextColor, 1000);
       level++;
-      setLevel(level);
-      console.log("Next level : " + level);
+      score++;
+      if(score > highScore) {
+        highScore = score;
+      }
+      showMessage(messages["watch"]);
+      showScore();
     }
   }
 }
